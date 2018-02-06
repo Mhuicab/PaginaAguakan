cbe282a1d9d0dee104dfdb279a86a9e125f459d3
Manuel Huicab <Manuel_Huicab@outlook.com>
2018-02-06 12:30:11 -0600

    cambios design

--- View/Home.php
+++ View/Home.php
@@ -8,71 +8,57 @@
   <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
 </head>
 <body>
-  <div class="header">
-    <div class="container-fluid">
-      <nav class="navbar navbar-default">
-        <div class="navbar-header">
-          <a class="navbar-brand glyphicon glyphicon-home" href="index.php"></a>
+  <div class="wrapper">
+    <a class="glyphicon glyphicon-home" href="index.php"></a>
+    <nav class="nav-main">
+      <div class="top">
+        <div class="header">
+          <div class="pull-right">
+            <div class="btn-close glyphicon glyphicon-log-out">Logout</div>
+          </div>
         </div>
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="#">Lecturitas</a></li>
-          <li><a href="Controller\ControllerRuta.php">Rutas</a></li>
-          <li><a href="#"></a></li>
+        <ul class="menu active">
+          <li class="active">
+            <a href="#">Lecturitas</a>
+          </li>
+          <li>
+            <a href="Controller\ControllerRuta.php">Rutas</a>
+          </li>
+          <li>
+            <a href="#">
+            </a>
+          </li>
         </ul>
-        <ul class="nav navbar-nav">
-          <form class="navbar-form navbar-left" action="index.html" method="post">
-            <div class="form-group">
-              <input type="text" class="form-control" placeholder="Buscar">
+      </div>
+    </nav>
+
+    <div class="header">
+      <div class="header-main">
+        <div class="box-left">
+          <div class="item bt-menu nav-main" href="index.php">
+            <div class="glyphicon glyphicon-home">
+              
             </div>
-            <button type="submit"class="btn btn-default">Buscar</button>
-          </form>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="View\Login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-        </ul>
-      </nav>
-      <div class="breadcrumb col-sm-3">
-        <?php echo $crumbs; ?>
+
+          </div>
+
+        </div>
+
       </div>
+
     </div>
 
 
-  </div>
 
-  <div class="content">
-    <div class="container">
-      <div class="table-responsive">
-        <table class="table table-bordered  ">
-          <thead class="table table-default">
-            <tr>
-              <td>ID</td>
-              <td>Nombre</td>
-              <td>Apellido</td>
-              <td>User</td>
-            </tr>
-          </thead>
-          <tbody>
-            <?php foreach ($listUser as $lusr): ?>
-              <tr>
-                <td><?php echo $lusr['Id'] ?></td>
-                <td><?php echo $lusr['Nombre'] ?></td>
-                <td><?php echo $lusr['Apellido'] ?></td>
-                <td><?php echo $lusr['Usuario']; ?></td>
-              </tr>
-            <?php endforeach; ?>
 
-          </tbody>
-        </table>
+
+
+
+    <div class="footer">
+      <div class="container">
+        <p class="text-muted logfin credit">Aguakan Manuel Huicab</p>
+
       </div>
-    </div>
-  </div>
-
-
-
-  <div class="footer">
-    <div class="container">
-      <p class="text-muted logfin credit">Aguakan Manuel Huicab</p>
-
     </div>
   </div>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-require_once 'Controller\ControllerLogin.php';
+require_once 'Controller\ControllerConsul.php';
 ?>
