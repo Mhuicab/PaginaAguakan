5ed5707fe9a15cb11126f3d3f95c4287c163a7bb
Manuel Huicab <Manuel_Huicab@outlook.com>
2018-02-06 12:30:11 -0600

    cambios design

--- Model/LoginModel.php
+++ Model/LoginModel.php
@@ -13,18 +13,9 @@ class LoginModel
       $this->db= ConectarDB::ConexionDB();
   }
 
-  public function login($Username, $pass){
+  public function login(){
 
     try {
-        $hash_password= hash('sha256', $pass);
-        $sqlLog=$this->db->query("SELECT  Id FROM `usuarioweb` WHERE `Usuario` =:Username AND 'Password'=:hash_password");
-        $sqlLog->blindParam("Username", $Username, PDO::PARAM_STR);
-        $sqlLog->blindParam("hash_password", $hash_password, PDO::PARAM_STR);
-        $sqlLog->execute();
-        $count = $sqlLog->rowCount();
-        $data=$sqlLog->fetch(PDO::FETCH_OBJ);
-        $db=null;
-        
 
     } catch (\Exception $e) {
 
